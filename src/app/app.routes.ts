bf461b6b5317fd38bee728a87f9e89a0062a339a
Mainara Monteiro Barbosa <mainaramonteiro0@gmail.com>
2026-09-04 20:17:49 -0300

    feat: estrutura de pastas, componentes e rotas da etapa 4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo: 'sobre',
+        pathMatch: 'full'
+    },
+    {
+    path: 'sobre',
+    loadComponent:() => import('./features/sobre/sobre').then(m => m.Sobre),
+    },
+    {
+        path: 'objetivo-profissional',
+        loadComponent:() => import('./features/objetivo-profissional/objetivo-profissional').then(m => m.ObjetivoProfissional),
+    },
+    {
+    path: 'habilidades',
+    loadComponent:() => import('./features/habilidades/habilidades').then(m => m.Habilidades),
+    },
+    {
+    path: 'formacao-academica',
+    loadComponent:() => import('./features/formacao-academica/formacao-academica').then(m => m.FormacaoAcademica),
+    },
+    { 
+    path: '**', 
+    redirectTo: 'sobre' 
+    }
+];
